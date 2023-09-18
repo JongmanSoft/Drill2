d1cc7ecd97af580542961571266d25578fa7f70c
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-09-18 11:34:43 +0900

    boy한테 반려포켓몬을 만들어주었다
    여자친구 리소스는 못찾겠어서
    귀여운 피카츄넣어줬당 ㅎㅎ
    근데 피카츄사진이 엄청커서..
    피봇의 중요성을 알게되었다

--- character_grass.py
+++ character_grass.py
@@ -3,14 +3,13 @@ from pico2d import *
 open_canvas()
 hide_lattice()
 
-x = 0
-
 grass = load_image('grass.png')
 boy = load_image('character.png')
-grass.draw(400,30)
-for i in range(0,10):
-    for j in range(0,10):
-        boy.draw_now(i*80 , j*60)
+pikachu = load_image('pikachu.png')
+
+grass.draw_now(400,30)
+boy.draw_now(400,80)
+pikachu.draw_now(100,120)
 
 delay(3)
 close_canvas()

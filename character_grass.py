edd91cd0ba4e2696476b589d9fdab28e27075123
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-09-18 11:44:01 +0900

    하늘에서 떨어지는 피카츄
    그리고 그걸 피하는 boy

--- character_grass.py
+++ character_grass.py
@@ -7,9 +7,15 @@ grass = load_image('grass.png')
 boy = load_image('character.png')
 pikachu = load_image('pikachu.png')
 
-grass.draw_now(400,30)
-boy.draw_now(400,80)
-pikachu.draw_now(100,120)
+i = 10
+
+while(i>0):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    boy.draw_now((10-i)*80,80)
+    pikachu.draw_now(i*80,i*60)
+    i = i-1
+    delay(0.05)
 
 delay(3)
 close_canvas()
